05ce2c2151d6dbcdf25ea8c692e5b656b3f396d3
Rachael <60798176+rachaelhrlm@users.noreply.github.com>
2022-03-24 20:14:28 +0000

    create title component (#7)

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from 'react';
-import { Card } from '../../component';
+import { Card, Title } from '../../component';
 
 export const Home: FunctionComponent = () => {
     return (
         <main className="bg-dark-blue-600 w-screen h-screen flex place-content-center place-items-center">
-            <Card></Card>
+            <Card>
+                <Title>How did we do?</Title>
+            </Card>
         </main>
     );
 };
