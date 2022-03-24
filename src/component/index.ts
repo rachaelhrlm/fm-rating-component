8ce5ff8607902be1e6bf3a414d3eb83e5e0c8cdd
Rachael Morris <rachael.morris@sky.uk>
2022-03-24 20:25:12 +0000

    create text component

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,2 +1,3 @@
 export * from './card';
+export * from './text';
 export * from './title';
--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,11 +1,14 @@
 import React, { FunctionComponent } from 'react';
-import { Card, Title } from '../../component';
+import { Card, Text, Title } from '../../component';
 
 export const Home: FunctionComponent = () => {
     return (
         <main className="bg-dark-blue-600 w-screen h-screen flex place-content-center place-items-center">
             <Card>
                 <Title>How did we do?</Title>
+                <Text>
+                    Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+                </Text>
             </Card>
         </main>
     );
