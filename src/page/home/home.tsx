f3b2a4b98a616c1c8ac86d00bfce0ce98ee87c6c
Rachael <60798176+rachaelhrlm@users.noreply.github.com>
2022-03-24 20:49:23 +0000

    create rating button component (#9)

--- src/page/home/home.tsx
+++ src/page/home/home.tsx
@@ -1,7 +1,8 @@
-import React, { FunctionComponent } from 'react';
-import { Card, Text, Title } from '../../component';
+import React, { FunctionComponent, useState } from 'react';
+import { Card, RatingButton, Text, Title } from '../../component';
 
 export const Home: FunctionComponent = () => {
+    const [rating, setRating] = useState<number>();
     return (
         <main className="bg-dark-blue-600 w-screen h-screen flex place-content-center place-items-center">
             <Card>
@@ -9,6 +10,13 @@ export const Home: FunctionComponent = () => {
                 <Text>
                     Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
                 </Text>
+                <section className="flex justify-between py-[20px]">
+                    <RatingButton onClick={(value: number) => setRating(value)} selected={rating} value={1} />
+                    <RatingButton onClick={(value: number) => setRating(value)} selected={rating} value={2} />
+                    <RatingButton onClick={(value: number) => setRating(value)} selected={rating} value={3} />
+                    <RatingButton onClick={(value: number) => setRating(value)} selected={rating} value={4} />
+                    <RatingButton onClick={(value: number) => setRating(value)} selected={rating} value={5} />
+                </section>
             </Card>
         </main>
     );
