105ed12821f8c64f3b477f2920625a7cb8a0f06d
Rachael Morris <rachael.morris@sky.uk>
2022-03-25 10:40:39 +0000

    fix class typo on rating button

--- src/component/ratingButton/ratingButton.tsx
+++ src/component/ratingButton/ratingButton.tsx
@@ -13,7 +13,7 @@ export const RatingButton: FunctionComponent<RatingButtonProps> = ({ onClick, se
         <button
             onClick={() => onClick(value)}
             className={classNames(
-                'h-[42px] w-[42px] rounded-full font-sans font-bold text-[14px] flex place-items-center justify-center cursor-pointer hover:bg-orange-500 hover:text-white transition-all duration-300 2xl:h-[51px] 2xl:w-[51px] 2x:text-[16px]',
+                'h-[42px] w-[42px] rounded-full font-sans font-bold text-[14px] flex place-items-center justify-center cursor-pointer hover:bg-orange-500 hover:text-white transition-all duration-300 2xl:h-[51px] 2xl:w-[51px] 2xl:text-[16px]',
                 { 'bg-gray-600 text-white': isSelected },
                 { 'bg-dark-blue-500 text-grey-500': !isSelected },
             )}>
