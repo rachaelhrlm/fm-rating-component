371fb924512ac244bef93926a1c13b3d35aece97
Rachael Morris <rachael.morris@sky.uk>
2022-03-25 09:39:42 +0000

    replace button container section with div

--- src/component/ratingCard/ratingCard.tsx
+++ src/component/ratingCard/ratingCard.tsx
@@ -13,13 +13,13 @@ export const RatingCard: FunctionComponent<RatingCardProps> = ({ onRatingClick, 
             <StarIcon />
             <Title>How did we do?</Title>
             <Text>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</Text>
-            <section className="flex justify-between py-[10px] 2xl:py-[0px]">
+            <div className="flex justify-between py-[10px] 2xl:py-[0px]">
                 <RatingButton onClick={onRatingClick} selected={rating} value={1} />
                 <RatingButton onClick={onRatingClick} selected={rating} value={2} />
                 <RatingButton onClick={onRatingClick} selected={rating} value={3} />
                 <RatingButton onClick={onRatingClick} selected={rating} value={4} />
                 <RatingButton onClick={onRatingClick} selected={rating} value={5} />
-            </section>
+            </div>
             <Button onClick={onSubmit}>Submit</Button>
         </Card>
     );
